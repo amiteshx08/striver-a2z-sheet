1f59cb10851e079dc5964f01bab622492f616323
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-05-14 22:17:57 +0530

    Star Pyramid solved

---
+++ pattern7.cpp
@@ -0,0 +1,23 @@
+// Star pyramid
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i){
+        // a seperate loop for space
+        for(int s = 0; s < n - i; ++s) {
+            cout << " ";
+        }
+        
+        // a seperate loop for stars
+        for(int j = 0; j < 2 * i - 1 ; ++j){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+}
