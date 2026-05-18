a5126d1495eff21a22e63a8611a7d036fcd29ab8
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-05-18 14:22:03 +0530

    Increasing number triangle solved

---
+++ pattern13.cpp
@@ -0,0 +1,17 @@
+// Increasing number triangle 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int r = 0;
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            r += 1;
+            cout << r << " ";
+        }
+        cout << "\n";
+    }
+}
