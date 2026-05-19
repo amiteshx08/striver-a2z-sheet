2783f4a02093d22d16e0918da295e4152b8b35c1
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-05-19 18:57:58 +0530

    Leetcode Rotate image solved

---
+++ leet48.cpp
@@ -0,0 +1,32 @@
+// Rotate Image
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotateImage(vector<vector<int> > &matrix) {
+    int n = matrix.size();
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = i + 1; j < n; ++j) {
+           swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    for(int i = 0; i < n; ++i) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+}
+
+int main() {
+    vector<vector<int> > matrix = {{1,2,3},
+    {4,5,6}, {7,8,9}};
+    rotateImage(matrix);
+}
