48eabf1afba3925a6d8e62a2bb356218c7a686a8
Amitesh Harsh Lal <amiteshharshlal@gmail.com>
2026-05-21 22:22:43 +0530

    Alpha Ramp solved

---
+++ pattern16.cpp
@@ -0,0 +1,17 @@
+// Alpha - Ramp 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) {
+        char pattern = 'A' + i;
+        for(int j = 0; j <= i; ++j) {
+            cout << pattern;
+        }
+        cout << "\n";
+    }
+}
